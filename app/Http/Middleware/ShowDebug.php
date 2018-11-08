f5ecccab592852b7bfea63eda957ab93e0d82b9f
john <john@scandinaver.org>
2018-11-08 04:55:54 +0400

    debugbar for ip

---
+++ app/Http/Middleware/ShowDebug.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Request;
+
+class ShowDebug
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (in_array(Request::ip(), ['127.0.0.1', '77.242.99.149'])) {
+            \Debugbar::enable();
+        }
+        else {
+            \Debugbar::disable();
+        }
+
+        return $next($request);
+    }
+}
